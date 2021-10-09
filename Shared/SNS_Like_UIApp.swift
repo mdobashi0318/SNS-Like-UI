4a1f2fbf70c31164dbee5f7ba942b84fd59f79d0
MasaharuDobashi <masaharu.dobashi@gmail.com>
2021-10-09 19:23:27 +0900

    Initial Commit

---
+++ Shared/SNS_Like_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNS_Like_UIApp.swift
+//  Shared
+//
+//  Created by 土橋正晴 on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct SNS_Like_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
