83929b6c2aadba147d33056345a1043a949258d0
MasaharuDobashi <masaharu.dobashi@gmail.com>
2021-10-09 23:06:17 +0900

    addButtonを追加

--- Shared/Screen/BaseView.swift
+++ Shared/Screen/BaseView.swift
@@ -9,25 +9,42 @@ import SwiftUI
 
 struct BasetView: View {
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-                }
-            Text("tab2")
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    
-                }
-            Text("tab3")
-                .tabItem {
-                    Image(systemName: "bell")
-                }
-            Text("tab4")
-                .tabItem {
-                    Image(systemName: "envelope")
-                }
+        ZStack {
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house")
+                    }
+                Text("tab2")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        
+                    }
+                Text("tab3")
+                    .tabItem {
+                        Image(systemName: "bell")
+                    }
+                Text("tab4")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                    }
+            }
+            addButton
         }
+    }
+    
+    
+    private var addButton: some View {
+        Button(action: {
+            print("Tap!")
+        }, label: {
+            Image(systemName: "plus")
+                .frame(width: 50, height: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(50 / 2)
+        })
+            .offset(x: UIScreen.main.bounds.width / 2.7, y: UIScreen.main.bounds.height / 3.0)
     }
 }
 
